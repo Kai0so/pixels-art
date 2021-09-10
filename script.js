1831f39b55a45896b7f4800154538c5a8d7aa095
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2021-09-10 17:11:03 -0300

    Adiciona requisito 2

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  const firstColor = document.getElementsByClassName('color')[0];
+  const secondColor = document.getElementsByClassName('color')[1];
+  const thirdColor = document.getElementsByClassName('color')[2];
+  const fourthColor = document.getElementsByClassName('color')[3];
+
+  firstColor.style.backgroundColor = 'red';
+  secondColor.style.backgroundColor = 'green';
+  thirdColor.style.backgroundColor = 'blue';
+  fourthColor.style.backgroundColor = 'yellow';
+};
