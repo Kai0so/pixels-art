3a0796d00116e8c752541b24745190a2241d553c
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2021-09-10 20:58:35 -0300

    Adiciona requisito 4

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-window.onload = function () {
-  const firstColor = document.getElementsByClassName('color')[0];
-  const secondColor = document.getElementsByClassName('color')[1];
-  const thirdColor = document.getElementsByClassName('color')[2];
-  const fourthColor = document.getElementsByClassName('color')[3];
+const firstColor = document.getElementsByClassName('color')[0];
+const secondColor = document.getElementsByClassName('color')[1];
+const thirdColor = document.getElementsByClassName('color')[2];
+const fourthColor = document.getElementsByClassName('color')[3];
 
-  firstColor.style.backgroundColor = 'black';
-  secondColor.style.backgroundColor = 'red';
-  thirdColor.style.backgroundColor = 'blue';
-  fourthColor.style.backgroundColor = 'yellow';
-};
+firstColor.style.backgroundColor = 'black';
+secondColor.style.backgroundColor = 'red';
+thirdColor.style.backgroundColor = 'blue';
+fourthColor.style.backgroundColor = 'yellow';
+
+const pixelBoard = document.getElementById('pixel-board');
+const pixelBoardRows = document.getElementsByClassName('pixel-board-rows');
+
+for (let i = 0; i < 5; i += 1) {
+  const row = document.createElement('div');
+  pixelBoard.appendChild(row).className = 'pixel-board-rows';
+}
+
+for (let j = 0; j < 5; j += 1) {
+  for (let k = 0; k < 5; k += 1) {
+    const newCell = document.createElement('div');
+    pixelBoardRows[k].appendChild(newCell).className = 'pixel';
+  }
+}
